278e58d4aae4ef2a6158328b0e02d900bae8c492
Yongtao Huang <yongtaoh2022@gmail.com>
2025-04-19 18:15:17 +0800

    Refactor PDF processor (#8)
    Refactor PDF processor
    
    Signed-off-by: Yongtao Huang <yongtaoh2022@gmail.com>

--- pdf2mind.py
+++ pdf2mind.py
@@ -7,26 +7,17 @@ from utils import model_selector, format_selector
 def main():
     args = cmd_parser()
     
-    pdf_name = str(args.pdf)
-    chunk_size = getattr(args, 'chunk_size', None)
-    overlap_size = getattr(args, 'overlap_size', None)
-    
-    if chunk_size is not None and overlap_size is not None:
-        chunks = PdfProcess.extract_pdf_chunks(pdf_name, int(chunk_size), int(overlap_size))
-    elif chunk_size is not None:
-        chunks = PdfProcess.extract_pdf_chunks(pdf_name, chunk_size=int(chunk_size))
-    elif overlap_size is not None:
-        chunks = PdfProcess.extract_pdf_chunks(pdf_name, overlap_size=int(overlap_size))
-    else:
-        chunks = PdfProcess.extract_pdf_chunks(pdf_name)
+    processor = PdfProcess()
+    chunks = processor.extract_pdf_chunks(args)
     print("The PDF has been split into", len(chunks), "parts")
     
     llm_model = model_selector(args)
     llm_model.loop_pdf_input(chunks)
     markdown_output = llm_model.get_md_result()
 
-    only_what = format_selector(args)
-    gen_all_mindmaps(markdown_output, pdf_name, only_what)
+    pdf_name = str(args.pdf)
+    only_what_format = format_selector(args)
+    gen_all_mindmaps(markdown_output, pdf_name, only_what_format)
 
 if __name__ == "__main__":
     main()
